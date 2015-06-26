752cb8b3f71682e55537ec404219e47167fab821
Matthew T Farley <matthewthomasfarley@gmail.com>
2015-06-26 09:20:25 -0400

    Completes assignment 25

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - Decimate all entries"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system 'clear'
+      decimate_entries!
+      main_menu
+    when 6
       puts "Goodbye!"
       exit(0)
     else
@@ -75,6 +80,14 @@ class MenuController
   def delete_entry(entry)
     @address_book.entries.delete(entry)
     puts "#{entry.name} has been deleted"
+  end
+
+  def decimate_entries!
+      @address_book.entries = []
+      #delete_entry(entries[0]) while entries.size > 0
+      #@address_book.entries.each_with_index do |entry, index|
+      #delete_entry(@address_book.entries[0])
+    #end
   end
 
   def edit_entry(entry)
